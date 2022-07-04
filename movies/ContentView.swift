3207bb2ebeb837ae958d0445d5bc65273e7a809d
danielferrer <danybarco@gmail.com>
2022-07-04 06:40:40 +0200

    Initial Commit

---
+++ movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movies
+//
+//  Created by Daniel Ferrer on 4/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Daniel Ferrer on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
