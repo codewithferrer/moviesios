82b1fbe830bb7d60d064ef689e9c39183b42ee9d
danielferrer <danybarco@gmail.com>
2022-08-12 06:57:00 +0200

    Add configuration and apiKey value

---
+++ services/Configuration.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration.swift
+//  movies
+//
+//  Created by Daniel Ferrer on 11/8/22.
+//
+
+import Foundation
+
+class Configuration {
+    
+    let apiKey: String? = Bundle.main.infoDictionary?["API_KEY"] as? String
+    
+}
