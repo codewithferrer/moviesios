6f93c8cd16a246c4e2f76934c9de5fd8ca5c0b2a
danielferrer <danybarco@gmail.com>
2022-08-19 14:50:06 +0200

    Add dummy test

--- moviesTests/moviesTests.swift
+++ moviesTests/moviesTests.swift
@@ -24,6 +24,8 @@ class moviesTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let v = true
+        XCTAssertTrue(v, "This value is not true")
     }
 
     func testPerformanceExample() throws {
